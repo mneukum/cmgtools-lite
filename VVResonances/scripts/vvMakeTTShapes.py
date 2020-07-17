3b5371f5466945441ed2bf0f8e08e751f8e5e351
IreneZoi <irene.zoi@cern.ch>
2020-07-17 16:35:44 +0200

    fixed non matching blank spaces

--- VVResonances/scripts/vvMakeTTShapes.py
+++ VVResonances/scripts/vvMakeTTShapes.py
@@ -217,7 +217,7 @@ def get2DHist(plts, lumi_):
   return histo2D_l1,histo2D_l2,histo2D
 
 def doFit(th1_projY,mjj_mean,mjj_error,N):
-
+  
   fitter=Fitter(['x'])
   fitter.erfexp2Gaus('model','x')
   projY.Rebin(2)
